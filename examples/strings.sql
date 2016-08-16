394a3f44543168499ab7fe35670eee00407e8225
Łukasz Hanuszczak <lukasz.hanuszczak@tensquaregames.com>
2016-08-16 13:32:38 +0200

    Extend the string examples with weakly-typed queries.

--- examples/strings.sql
+++ examples/strings.sql
@@ -1,5 +1,11 @@
 SELECT JsonQuery('{ "foo": "bar baz" }', 'foo');
 SELECT JsonQueryString('{ "foo": "bar baz" }', 'foo');
+SELECT JsonQueryUnquoted('{ "foo": "bar baz" }', 'foo');
+
+SELECT JsonQueryString('{ "foo": 10 }', 'foo');
+SELECT JsonQueryUnquoted('{ "foo": 10 }', 'foo');
 
 SELECT JsonQuery('{ "foo": true }', 'foo')::boolean;
 SELECT JsonQueryString('{ "foo": "true" }', 'foo')::boolean;
+SELECT JsonQueryUnquoted('{ "foo": true }', 'foo')::boolean;
+SELECT JsonQueryUnquoted('{ "foo": "true" }', 'foo')::boolean;
