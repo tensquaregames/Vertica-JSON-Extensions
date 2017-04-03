c28b80d449603044a8fc6e74329e0bf99964262a
Pawel Michalak <pawel.michalak@tensquaregames.com>
2017-04-04 00:23:09 +0200

    Add long varchar examples.

--- examples/unnest.sql
+++ examples/unnest.sql
@@ -11,6 +11,10 @@ SELECT JsonArrayUnnest('["foo", true, false, "bar"]') OVER ();
 SELECT JsonArrayUnnestStrings('["foo", true, false, "bar"]') OVER ();
 SELECT JsonArrayUnnestUnquoted('["foo", true, false, "bar"]') OVER ();
 
+SELECT JsonArrayUnnest('["foo", true, 1, 0.1]'::long varchar) OVER ();
+SELECT JsonArrayUnnestStrings('["foo", true, 1, 0.1]'::long varchar) OVER ();
+SELECT JsonArrayUnnestUnquoted('["foo", true, 1, 0.1]'::long varchar) OVER ();
+
 \set object_json '\'[{ "foo": 4, "bar": true }, { "foo": 8, "bar": false }, { "foo": 16, "bar": true }]\''
 SELECT JsonQuery(object, 'foo')
 FROM (SELECT JsonArrayUnnest(:object_json) OVER () AS object)
