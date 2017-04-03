1b2a3fcbf52e3a30bf8f47a645eb632780449c98
Pawel Michalak <pawel.michalak@tensquaregames.com>
2017-04-03 20:22:06 +0200

    typo

--- examples/dots.sql
+++ examples/dots.sql
@@ -3,4 +3,4 @@ SELECT JsonQuery('{ "foo.bar": true, "foo": { "bar": false } }', 'foo\.bar');
 
 SELECT JsonQuery('{ "": true }', '');
 SELECT JsonQuery('{ "": { "foo": true } }', '.foo');
-SELECT JsonQuerY('{ "": { "": { ".": true } } }', '..\.');
+SELECT JsonQuery('{ "": { "": { ".": true } } }', '..\.');
